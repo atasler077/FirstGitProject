f782c7b00424cec77e32d010d758df47e7fc6d16
Anthony Tasler <anthony_tasler@intuit.com>
2021-01-23 12:48:17 -0800

    added motivation pharse

--- greetings.py
+++ greetings.py
@@ -4,3 +4,7 @@ print ("Welcome my Friend")
 # show some cool Greetings image
 
 # Ask the user what he/she wants to drink
+
+if you can dream it - you can do it!
+
+                                        
